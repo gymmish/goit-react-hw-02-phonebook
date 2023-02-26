aea832c5b27d0a64b33a26161966dba6a0be7fc7
AS <88048844+gymmish@users.noreply.github.com>
2023-02-26 23:23:46 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import PhoneForm from './PhoneForm/PhoneForm';
+import MyContacts from './Contacts/Contacts';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  addContact = ({ name }) => {
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, { id: nanoid(), name }],
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <PhoneForm onSubmit={this.addContact} contacts={this.state.contacts} />
+
+        <MyContacts />
+      </div>
+    );
+  }
+}
